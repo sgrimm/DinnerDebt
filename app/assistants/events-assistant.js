650fea4c97dde892bf1dad25582fc4bfe0769c37
Steven Grimm <koreth@gmail.com>
2009-09-01 08:14:35 -0700

    Use Prototype-style class definitions for people, events assistants

--- app/assistants/events-assistant.js
+++ app/assistants/events-assistant.js
@@ -1,136 +1,120 @@
-function EventsAssistant(stageAssistant) {
-	/* this is the creator function for your scene assistant object. It will be passed all the 
-	   additional parameters (after the scene name) that were passed to pushScene. The reference
-	   to the scene controller (this.controller) has not be established yet, so any initialization
-	   that needs the scene controller should be done in the setup function below. */
-	
-	this.stageAssistant = stageAssistant;
-	this.lastItemTapped = null;
-}
+var EventsAssistant = Class.create({
+	initialize: function(stageAssistant) {
+		this.stageAssistant = stageAssistant;
+		this.lastItemTapped = null;
+	},
 
-EventsAssistant.prototype.setup = function() {
-	/* this function is for setup tasks that have to happen when the scene is first created */
-	this.mgr = new EventManager(this.controller);
+	setup: function() {
+		this.mgr = new EventManager(this.controller);
 
-	/* use Mojo.View.render to render view templates and add them to the scene, if needed. */
-	
-	/* setup widgets here */
-	this.controller.setupWidget('eventsList',
-		{
-			itemTemplate : 'events/listitem',
-			itemsCallback : this.itemsCallback.bind(this),
-			formatters : {
-				total : this.formatTotal.bind(this),
-				date : this.formatDate.bind(this)
-			},
-			addItemLabel : $L('Add Event'),
-		});
-
-	this.controller.setupWidget(Mojo.Menu.viewMenu, {}, {
-			items: [
-				{
-					toggleCmd: 'events',
-					items: [
-						{ label: 'Events', command: 'events', width: 160 },
-						{ label: 'People', command: 'people', width: 160 },
-					],
+		this.controller.setupWidget('eventsList',
+			{
+				itemTemplate : 'events/listitem',
+				itemsCallback : this.itemsCallback.bind(this),
+				formatters : {
+					total : this.formatTotal.bind(this),
+					date : this.formatDate.bind(this)
 				},
-			],
+				addItemLabel : $L('Add Event'),
+			});
+
+		this.controller.setupWidget(Mojo.Menu.viewMenu, {}, {
+				items: [
+					{
+						toggleCmd: 'events',
+						items: [
+							{ label: 'Events', command: 'events', width: 160 },
+							{ label: 'People', command: 'people', width: 160 },
+						],
+					},
+				],
+			});
+
+		this.eventsList = this.controller.get('eventsList');
+		this.mgr.listen('eventsList', Mojo.Event.listTap, this.handleEventTap.bind(this));
+		this.mgr.listen('eventsList', Mojo.Event.listAdd, this.handleAddTap.bind(this));
+	},
+
+	itemsCallback: function(listWidget, offset, count) {
+		try {
+			DDEvent.getList(
+				function(list) {
+					listWidget.mojo.noticeUpdatedItems(offset, list.slice(offset,offset+count));
+				});
+		}
+		catch (e) {
+			Mojo.Log.error("Can't populate event list", e);
+			throw e;
+		}
+	},
+
+	formatTotal: function(val, obj) {
+		return Mojo.Format.formatCurrency(val / 100.0, 2);
+	},
+
+	formatDate: function(val, obj) {
+		return Mojo.Format.formatDate(val, {date:'short'});
+	},
+
+	/**
+	 * Does initialization that's needed whenever the scene is active.
+	 * For example, key handlers that are observing the document.
+	 */
+	activate: function(event) {
+		var mojo = this.eventsList.mojo;
+		DDEvent.getListLength(function(length) {
+			mojo.setLengthAndInvalidate(length);
+			mojo.revealItem(this.lastItemTapped ? this.lastItemTapped : length - 1);
 		});
 
-	/* add event handlers to listen to events from widgets */
-	this.eventsList = this.controller.get('eventsList');
-	this.mgr.listen('eventsList', Mojo.Event.listTap, this.handleEventTap.bind(this));
-	this.mgr.listen('eventsList', Mojo.Event.listAdd, this.handleAddTap.bind(this));
-}
-
-EventsAssistant.prototype.itemsCallback = function(listWidget, offset, count){
-	try {
-		DDEvent.getList(
-			function(list) {
-				listWidget.mojo.noticeUpdatedItems(offset, list.slice(offset,offset+count));
-			});
-	}
-	catch (e) {
-		Mojo.Log.error("Can't populate event list", e);
-		throw e;
-	}
-};
-
-EventsAssistant.prototype.formatTotal = function(val, obj) {
-	return Mojo.Format.formatCurrency(val / 100.0, 2);
-};
-
-EventsAssistant.prototype.formatDate = function(val, obj) {
-	return Mojo.Format.formatDate(val, {date:'short'});
-};
-
-/**
- * Does initialization that's needed whenever the scene is active.
- * For example, key handlers that are observing the document.
- */
-EventsAssistant.prototype.activate = function(event) {
-	var mojo = this.eventsList.mojo;
-	DDEvent.getListLength(function(length) {
-		mojo.setLengthAndInvalidate(length);
-		mojo.revealItem(this.lastItemTapped ? this.lastItemTapped : length - 1);
-	});
-
-	try {
-		this.mgr.activateHandlers();
-	}
-	catch (e) {
-		Mojo.Log.warn(e);
-	}
-}
-
-
-EventsAssistant.prototype.deactivate = function(event) {
-	try {
-		this.mgr.deactivateHandlers();
-	}
-	catch (e) {
-		Mojo.Log.warn(e);
-	}
-	/* remove any event handlers you added in activate and do any other cleanup that should happen before
-	   this scene is popped or another scene is pushed on top */
-}
-
-EventsAssistant.prototype.cleanup = function(event) {
-	/* this function should do any cleanup needed before the scene is destroyed as 
-	   a result of being popped off the scene stack */
-}
-
-/**
- * Handles a tap on the "Events" header (switches to People view.)
- */
-EventsAssistant.prototype.handleCommand = function(event) {
-	this.controller = Mojo.Controller.stageController.activeScene();
-	if (event.type == Mojo.Event.command) {
-		switch (event.command) {
-			case 'people':
-				this.controller.stageController.swapScene('people', this.stageAssistant);
-				break;
+		try {
+			this.mgr.activateHandlers();
 		}
-	}
-}
+		catch (e) {
+			Mojo.Log.warn(e);
+		}
+	},
 
-/**
- * Handles a tap on the "Add Event" button.
- */
-EventsAssistant.prototype.handleAddTap = function(event) {
-	this.lastItemTapped = null;
-	this.controller.stageController.pushScene('editevent', null, this.stageAssistant);
-}
+	deactivate: function(event) {
+		try {
+			this.mgr.deactivateHandlers();
+		}
+		catch (e) {
+			Mojo.Log.warn(e);
+		}
+	},
 
-/**
- * Handles a tap on the event list.
- */
-EventsAssistant.prototype.handleEventTap = function(event) {
-	this.lastItemTapped = event.index;
-	event.item.load(function(ddEvent) {
-		Mojo.Log.info("Post-load callback for event", ddEvent.id);
-		this.controller.stageController.pushScene('editevent', ddEvent,
-												this.stageAssistant);
-	}.bind(this));
-}
+	cleanup: function(event) { },
+
+	handleCommand: function(event) {
+		this.controller = Mojo.Controller.stageController.activeScene();
+		if (event.type == Mojo.Event.command) {
+			switch (event.command) {
+				case 'people':
+					this.controller.stageController.swapScene('people', this.stageAssistant);
+					break;
+			}
+		}
+	},
+
+	/**
+	 * Handles a tap on the "Add Event" button.
+	 */
+	handleAddTap: function(event) {
+		this.lastItemTapped = null;
+		this.controller.stageController.pushScene('editevent', null, this.stageAssistant);
+	},
+
+	/**
+	 * Handles a tap on the event list.
+	 */
+	handleEventTap: function(event) {
+		this.lastItemTapped = event.index;
+		event.item.load(function(ddEvent) {
+			Mojo.Log.info("Post-load callback for event", ddEvent.id);
+			this.controller.stageController.pushScene('editevent', ddEvent,
+													this.stageAssistant);
+		}.bind(this));
+	},
+
+});
--- app/assistants/people-assistant.js
+++ app/assistants/people-assistant.js
@@ -1,149 +1,140 @@
-function PeopleAssistant(stageAssistant) {
-	/* this is the creator function for your scene assistant object. It will be passed all the 
-	   additional parameters (after the scene name) that were passed to pushScene. The reference
-	   to the scene controller (this.controller) has not be established yet, so any initialization
-	   that needs the scene controller should be done in the setup function below. */
-	this.stageAssistant = stageAssistant;
-}
+var PeopleAssistant = Class.create({
+	initialize: function(stageAssistant) {
+		this.stageAssistant = stageAssistant;
+	},
 
-PeopleAssistant.prototype.setup = function() {
-	/* this function is for setup tasks that have to happen when the scene is first created */
-	this.mgr = new EventManager(this.controller);
+	setup: function() {
+		this.mgr = new EventManager(this.controller);
 
-	/* use Mojo.View.render to render view templates and add them to the scene, if needed. */
+		this.controller.setupWidget("peopleList",
+			{
+				itemTemplate: "people/listitem",
+				addItemLabel: $L("Add Person"),
+				swipeToDelete: true,
+				reorderable: true,
+				itemsCallback : this.itemsCallback.bind(this),
+				formatters : {
+					balance : this.formatBalance.bind(this),
+					name : this.formatName.bind(this),
+				}
+			});
 	
-	/* setup widgets here */
-	this.controller.setupWidget("peopleList",
-		{
-			itemTemplate: "people/listitem",
-			addItemLabel: $L("Add Person"),
-			swipeToDelete: true,
-			reorderable: true,
-			itemsCallback : this.itemsCallback.bind(this),
-			formatters : {
-				balance : this.formatBalance.bind(this),
-				name : this.formatName.bind(this),
+		this.viewMenuModel = {
+			visible: true,
+			items: [{
+				toggleCmd: 'people',
+				items: [
+					{ label: 'Events', command: 'events', width: 120 },
+					{ label: 'People', command: 'people', width: 120 },
+				]
+			},{
+				items: [
+					{ label: 'Sort', submenu: 'sort-menu' }
+				]
+			}]
+		};
+	
+		this.sortMenuModel = {
+			label: $L('Sort By'),
+			items: [
+				{ label: $L('Name'), command: 'name' },
+				{ label: $L('Balance'), command: 'balance' },
+				{ label: $L('Manual'), command: 'manual' }
+			]};
+	
+		this.controller.setupWidget(Mojo.Menu.viewMenu,
+			{
+				spacerHeight: 0,
+				menuClass: 'no-fade'
+			},
+			this.viewMenuModel);
+		this.controller.setupWidget('sort-menu', undefined, this.sortMenuModel);
+
+		this.mgr.listen('peopleList', Mojo.Event.listAdd, this.handleListAdd.bind(this));
+		this.mgr.listen('peopleList', Mojo.Event.listReorder,
+						this.handleListReorder.bind(this));
+
+		this.peopleListWidget = this.controller.get('peopleList');
+	},
+
+	itemsCallback: function(listWidget, offset, count) {
+		Person.getList(this.stageAssistant.personSortOrder,
+			function(list) {
+				listWidget.mojo.noticeUpdatedItems(offset, list.slice(offset, offset+count));
+			});
+	},
+
+	formatBalance: function(val, obj) {
+		return Mojo.Format.formatCurrency(val / 100.0, 2);
+	},
+
+	formatName: function(val, obj) {
+		return '<span class="dd-balance-' +
+				(obj.balance < 0 ? 'negative' : 'positive') +
+				'">' + val + '</span>';
+	},
+
+	activate: function(event) {
+		this.mgr.activateHandlers();
+	},
+
+	deactivate: function(event) {
+		this.mgr.deactivateHandlers();
+	},
+
+	cleanup: function(event) { },
+
+	/**
+	 * Handles a command from a system menu.
+	 */
+	handleCommand: function(event) {
+		if (event.type == Mojo.Event.command) {
+			switch (event.command) {
+				case 'name':
+					this.stageAssistant.setPersonSortOrder(Person.SORT_NAME);
+					break;
+				case 'balance':
+					this.stageAssistant.setPersonSortOrder(Person.SORT_BALANCE);
+					break;
+				case 'manual':
+					this.stageAssistant.setPersonSortOrder(Person.SORT_MANUAL);
+					break;
+
+				case 'events':
+					this.controller.stageController.swapScene('events', this.stageAssistant);
+					return;
 			}
-		});
-	
-	this.viewMenuModel = {
-		visible: true,
-		items: [{
-			toggleCmd: 'people',
-			items: [
-				{ label: 'Events', command: 'events', width: 120 },
-				{ label: 'People', command: 'people', width: 120 },
-			]
-		},{
-			items: [
-				{ label: 'Sort', submenu: 'sort-menu' }
-			]
-		}]
-	};
-	
-	this.sortMenuModel = {
-		label: $L('Sort By'),
-		items: [
-			{ label: $L('Name'), command: 'name' },
-			{ label: $L('Balance'), command: 'balance' },
-			{ label: $L('Manual'), command: 'manual' }
-		]};
-	
-	this.controller.setupWidget(Mojo.Menu.viewMenu,
-		{
-			spacerHeight: 0,
-			menuClass: 'no-fade'
-		},
-		this.viewMenuModel);
-	this.controller.setupWidget('sort-menu', undefined, this.sortMenuModel);
-
-	/* add event handlers to listen to events from widgets */
-	this.mgr.listen('peopleList', Mojo.Event.listAdd, this.handleListAdd.bind(this));
-	this.mgr.listen('peopleList', Mojo.Event.listReorder,
-					this.handleListReorder.bind(this));
-
-	this.peopleListWidget = this.controller.get('peopleList');
-}
-
-PeopleAssistant.prototype.itemsCallback = function(listWidget, offset, count) {
-	Person.getList(this.stageAssistant.personSortOrder,
-		function(list) {
-			listWidget.mojo.noticeUpdatedItems(offset, list.slice(offset, offset+count));
-		});
-};
-
-PeopleAssistant.prototype.formatBalance = function(val, obj) {
-	return Mojo.Format.formatCurrency(val / 100.0, 2);
-};
-
-PeopleAssistant.prototype.formatName = function(val, obj) {
-	return '<span class="dd-balance-' +
-			(obj.balance < 0 ? 'negative' : 'positive') +
-			'">' + val + '</span>';
-}
-
-PeopleAssistant.prototype.activate = function(event) {
-	this.mgr.activateHandlers();
-}
-
-
-PeopleAssistant.prototype.deactivate = function(event) {
-	this.mgr.deactivateHandlers();
-}
-
-PeopleAssistant.prototype.cleanup = function(event) {
-	/* this function should do any cleanup needed before the scene is destroyed as 
-	   a result of being popped off the scene stack */
-}
-
-/**
- * Handles a command from a system menu.
- */
-PeopleAssistant.prototype.handleCommand = function(event) {
-	if (event.type == Mojo.Event.command) {
-		switch (event.command) {
-			case 'name':
-				this.stageAssistant.setPersonSortOrder(Person.SORT_NAME);
-				break;
-			case 'balance':
-				this.stageAssistant.setPersonSortOrder(Person.SORT_BALANCE);
-				break;
-			case 'manual':
-				this.stageAssistant.setPersonSortOrder(Person.SORT_MANUAL);
-				break;
-
-			case 'events':
-				this.controller.stageController.swapScene('events', this.stageAssistant);
-				return;
 		}
-	}
 
-	this.peopleListWidget.mojo.invalidateItems(0);
-}
-
-/**
- * Handles a tap on the "add person" list item.
- */
-PeopleAssistant.prototype.handleListAdd = function(event) {
-	this.controller.showDialog({
-		template: 'people/add-person-dialog',
-		assistant: new AddPersonDialogAssistant(this),
-	});
-}
-
-/**
- * Handles the user dragging an item on the person list to reorder it.
- */
-PeopleAssistant.prototype.handleListReorder = function(event) {
-	if (this.stageAssistant.personSortOrder == Person.SORT_MANUAL) {
-		Person.reposition(event.fromIndex, event.toIndex);
-	} else {
-		// Reset the list order
-		// XXX - should just disable repositioning when sort order
-		// is not Manual
 		this.peopleListWidget.mojo.invalidateItems(0);
-	}
-}
+	},
+
+	/**
+	 * Handles a tap on the "add person" list item.
+	 */
+	handleListAdd: function(event) {
+		this.controller.showDialog({
+			template: 'people/add-person-dialog',
+			assistant: new AddPersonDialogAssistant(this),
+		});
+	},
+
+	/**
+	 * Handles the user dragging an item on the person list to reorder it.
+	 */
+	handleListReorder: function(event) {
+		if (this.stageAssistant.personSortOrder == Person.SORT_MANUAL) {
+			Person.reposition(event.fromIndex, event.toIndex);
+		} else {
+			// Reset the list order
+			// XXX - should just disable repositioning when sort order
+			// is not Manual
+			this.peopleListWidget.mojo.invalidateItems(0);
+		}
+	},
+
+});
+
 
 /**
  * Dialog assistant for the "Add Person" dialog.
@@ -183,7 +174,6 @@ var AddPersonDialogAssistant = Class.create({
 	},
 	
 	handleOkTap: function(event) {
-		try {
 		if (this.nameModel.value) {
 			var person = new Person(0, this.nameModel.value, 0);
 			person.add();
@@ -198,6 +188,5 @@ var AddPersonDialogAssistant = Class.create({
 		}
 
 		this.widget.mojo.close();
-	} catch (e) { Mojo.Log.error(e); throw e; }
 	},
 });
